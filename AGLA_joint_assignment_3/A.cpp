454510dff1a8f6d8270a861692aba8175c195209
MagicWinnie <magicwinnie@yandex.ru>
2023-04-30 10:47:49 +0300

    Added plotting to AGLA joint 3 task 1

--- AGLA_joint_assignment_3/A.cpp
+++ AGLA_joint_assignment_3/A.cpp
@@ -2,7 +2,10 @@
 // d.okoneshnikov@innopolis.university
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include <cmath>
+#define USE_GNUPLOT 0
+#define GNUPLOT_NAME "gnuplot -persist"
 
 using namespace std;
 
@@ -56,6 +59,44 @@ int main(void)
     for (int i = 0; i <= N; i++)
         cout << K_arr[i] << ' ';
     cout << '\n';
+
+#if (defined(WIN32) || defined(_WIN32)) && USE_GNUPLOT
+    FILE *pipe = _popen(GNUPLOT_NAME, "w");
+#elif USE_GNUPLOT
+    FILE *pipe = popen(GNUPLOT_NAME, "w");
+#endif
+#if USE_GNUPLOT
+    // Draw v(t), k(t)
+    fprintf(pipe, "%s\n", "set terminal png size 1920, 1080 font \"Calibri,24\"");
+    fprintf(pipe, "%s\n", "set output 'v_t__k_t.png'");
+    fprintf(pipe, "%s\n", "set title \"Predator-Prey Model\"");
+    fprintf(pipe, "%s\n", "set key noautotitle");
+    fprintf(pipe, "%s\n", "set autoscale xy");
+    fprintf(pipe, "%s\n", "set offsets 0.05, 0.05, 0.05, 0.05");
+    fprintf(pipe, "%s\n", "set xlabel \"t\"");
+    fprintf(pipe, "%s\n", "set ylabel \"v(t), k(t)\"");
+    ofstream out("v_t__k_t.txt");
+    for (int i = 0; i <= N; i++)
+        out << T_arr[i] << ' ' << V_arr[i] << ' ' << K_arr[i] << '\n';
+    out.close();
+    fprintf(pipe, "%s\n", "plot \"v_t__k_t.txt\" using 1:2 title 'v(t)' w linespoints pointtype 7, \"v_t__k_t.txt\" using 1:3 title 'k(t)' w linespoints pointtype 7");
+    fflush(pipe);
+    // Draw v(k)
+    fprintf(pipe, "%s\n", "set output 'v_k.png'");
+    fprintf(pipe, "%s\n", "set xlabel \"k\"");
+    fprintf(pipe, "%s\n", "set ylabel \"v\"");
+    out.open("v_k.txt");
+    for (int i = 0; i <= N; i++)
+        out << V_arr[i] << ' ' << K_arr[i] << '\n';
+    out.close();
+    fprintf(pipe, "%s\n", "plot \"v_k.txt\" using 1:2 title 'v(k)' w linespoints pointtype 7");
+    fflush(pipe);
+#endif
+#if (defined(WIN32) || defined(_WIN32)) && USE_GNUPLOT
+    _pclose(pipe);
+#elif USE_GNUPLOT
+    pclose(pipe);
+#endif
 
     // Delete arrays
     delete[] T_arr;
